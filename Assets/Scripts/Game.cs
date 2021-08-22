6d65c28a5f9d7bd3bf3ab73530dac6e8938d4912
vladovichus <vladovichus@gmail.com>
2021-08-22 20:59:14 +0300

    grid limits

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,15 @@ public class Game : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool CheckInsideGrid(Vector2 pos)
+    {
+        return (int) pos.x >= 0 && (int) pos.x < gridWidth && (int) pos.y >= 0;
+    }
+
+    public Vector2 Round(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
     }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -6,11 +6,10 @@ public class Tetromino : MonoBehaviour
 {
     private float fall = 0;
     public float fallSpeed = 1;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -23,21 +22,52 @@ public class Tetromino : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(1, 0, 0 );
+            transform.position += new Vector3(1, 0, 0);
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-1,0,0);
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate(0, 0, 90);
+            if (!CheckIsValidPosition())
+            {
+                transform.Rotate(0,0,-90);
+            }
+            
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
-            transform.position += new Vector3(0,-1,0);
+            transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
+            if (!CheckIsValidPosition())
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
         }
     }
-    
+
+    bool CheckIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+            if (FindObjectOfType<Game>().CheckInsideGrid(pos) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
